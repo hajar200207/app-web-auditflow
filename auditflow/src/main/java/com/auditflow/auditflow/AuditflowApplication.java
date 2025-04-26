772b442c1a8172d00bfe7dc79a45ec48f397f362
hajar200207 <ouadihajar1234@gmail.com>
2025-04-26 15:39:40 +0200

    first commit

---
+++ auditflow/src/main/java/com/auditflow/auditflow/AuditflowApplication.java
@@ -0,0 +1,13 @@
+package com.auditflow.auditflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditflowApplication.class, args);
+	}
+
+}
---
+++ auditflow/src/test/java/com/auditflow/auditflow/AuditflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auditflow.auditflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
