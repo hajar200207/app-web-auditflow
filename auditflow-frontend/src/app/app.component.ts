2e646ebefabe369a39f0a2b7869e0a2d99cd51f9
Naima AIT TALEB <naimaaittaleb220gmail.com>
2025-05-05 14:30:45 +0100

    login index

--- auditflow-frontend/src/app/app.component.ts
+++ auditflow-frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'auditflow-frontend';
+
+  showContactInfo() {
+    alert("Contact Information:\nEmail: contact@excellencehub.ma\nPhone: +212 666 40 49 00\nAddress: Rabat, MOROCCO");
+  }
 }
